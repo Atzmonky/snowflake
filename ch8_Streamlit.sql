e0b439de7872bc65497f472e71316df39d2bfb7f
Atzmonky <Atzmonky@gmail.com>
2022-10-20 12:08:52 +0200

    Update ch8_Streamlit.sql

--- ch8_Streamlit.sql
+++ ch8_Streamlit.sql
@@ -1,7 +1,7 @@
-// Creating a Database
+-- Creating a Database
 CREATE OR REPLACE DATABASE atzmon_db;
 
-// Creating a Warehouse
+-- Creating a Warehouse
 CREATE WAREHOUSE snowflake_wh
 WITH
   WAREHOUSE_SIZE = XSMALL
@@ -11,30 +11,30 @@ WITH
   STATEMENT_QUEUED_TIMEOUT_IN_SECONDS = 300
   STATEMENT_TIMEOUT_IN_SECONDS = 600;
 
-// Environment Configuration
+-- Environment Configuration
 USE WAREHOUSE snowflake_wh;
 USE DATABASE atzmon_db;
 USE SCHEMA public;
 
-// Creating a CSV file format
+-- Creating a CSV file format
 CREATE OR REPLACE FILE FORMAT ch8_csv
   type = 'CSV'
   field_delimiter = ','
   skip_header = 1;
   
-// Creating a Stage
+-- Creating a Stage
 CREATE OR REPLACE STAGE ch8_stg
   file_format = ch8_csv
   url = 's3://frostyfridaychallenges/challenge_8/payments.csv';
 
-// List objects in stage
+-- List objects in stage
 LIST @ch8_stg;
 
-// Checking the CSV in stage
+-- Checking the CSV in stage
 SELECT $1,$2, $3, $4
 FROM @ch8_stg (file_format => ch8_csv);
 
-// Creating a table and table schema  
+-- Creating a table and table schema  
 CREATE OR REPLACE TABLE ch8_tbl(
     id VARCHAR,
     payment_date VARCHAR,
@@ -42,7 +42,7 @@ CREATE OR REPLACE TABLE ch8_tbl(
     amount NUMBER
     );
   
-// Copy data to newly created table  
+-- Copy data to newly created table  
 COPY INTO ch8_tbl 
 FROM @ch8_stg;
 
